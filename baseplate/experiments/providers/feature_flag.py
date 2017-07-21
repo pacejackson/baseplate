7e9e882d365999d07fca5dd51c0558f07412acf2
Andrew Boyle <andrew.boyle@reddit.com>
2017-07-21 14:29:42 -0700

    Add documentation for FeatureFlag experiments

--- baseplate/experiments/providers/feature_flag.py
+++ baseplate/experiments/providers/feature_flag.py
@@ -7,6 +7,14 @@ from .r2 import R2Experiment
 
 
 class FeatureFlag(R2Experiment):
+    """ An experiment with a single variant "active" that does not log
+    bucketing events to the event pipeline.  Use this type of experiment if
+    you just want to controll access to a feature but do not want to run an
+    actual experiment.  For example:
+
+    1. Slowly rolling out a new feature to a % of users
+    2. Restricting a new feature to certain subreddits
+    """
 
     def should_log_bucketing(self):
         return False
