0d986801c8bee0500616efcb85b7a7d1b29edad7
Andrew Boyle <andrew.boyle@reddit.com>
2017-07-21 14:40:57 -0700

    parse_experiment documentation

--- baseplate/experiments/providers/__init__.py
+++ baseplate/experiments/providers/__init__.py
@@ -14,6 +14,37 @@ logger = logging.getLogger(__name__)
 
 
 def parse_experiment(config):
+    """ Factory method that parses an experiment config dict and returns an
+    appropriate Experiment class.
+
+    The config dict is expected to have the following format:
+
+        {
+            "id": Integer experiment ID, should be unique for each experiment.
+            "name": String experiment name, should be unique for each
+                experiment.
+            "owner": The group or individual that owns this experiment.
+            "expires": Date when this experiment expires as UTC epoch seconds.
+            "enabled": Optional value, defaults to True.  If set to False,
+                the experiment will be disabled and calls to experiment.variant
+                will always return None and will not log bucketing events to
+                the event pipeline.
+            "global_override": Optional value.  If this is set, calls to
+                experiment.variant will always return the override value and
+                will not log bucketing events to the event pipeline.
+            "type": String specifying the type of experiment to run.  If this
+                value is not reconized, the experiment will be considered
+                disabled.
+            "experiment": The experiment config dict for the specific type of
+                experiment.  The format of this is determined by the specific
+                experiment type.
+        }
+
+    :param dict config: Configuration dict for the experiment you wish to run.
+    :rtype baseplate.experiments.providers.base.Experiment:
+    :return: A subclass of :py:class:`Experiment` for the given experiment
+        type.
+    """
     experiment_type = config["type"].lower()
     experiment_id = config["id"]
     assert isinstance(experiment_id, int)
