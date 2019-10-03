c7f3dde99599a605654125df14d309eb0742a247
Andrew Boyle <andrew.boyle@reddit.com>
2019-10-02 17:26:17 -0700

    fmt

--- baseplate/frameworks/queue_consumer/__init__.py
+++ baseplate/frameworks/queue_consumer/__init__.py
@@ -4,10 +4,4 @@ from baseplate.frameworks.queue_consumer.deprecated import Handler
 from baseplate.frameworks.queue_consumer.deprecated import KombuConsumer
 from baseplate.frameworks.queue_consumer.deprecated import WorkQueue
 
-__all__ = [
-    "BaseKombuConsumer",
-    "consume",
-    "Handler",
-    "KombuConsumer",
-    "WorkQueue",
-]
+__all__ = ["BaseKombuConsumer", "consume", "Handler", "KombuConsumer", "WorkQueue"]
